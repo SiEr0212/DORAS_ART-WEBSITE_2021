6c078177a240b809c01007a4092c2e238b0f8ec3
SiEr0212 <simonertel@gmx.de>
2021-03-24 23:44:37 +0100

    added resizer function in index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 window.onload = function () {
-    
+    var ImageMap = function (map, img) {
+            var n,
+                areas = map.getElementsByTagName('area'),
+                len = areas.length,
+                coords = [],
+                previousWidth = 1093;
+            for (n = 0; n < len; n++) {
+                coords[n] = areas[n].coords.split(',');
+            }
+            
     imageMap.resize();
     return;
 }
